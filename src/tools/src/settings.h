a2e870a37897d2ccf04b489725bf147462bc5137
Wouter Deconinck <wdconinc@gmail.com>
2023-08-07 21:20:26 -0500

    fix: rm unused TEve* headers from various utilities (#13)
    ### Briefly, what does this PR introduce?
    This removes several unused TEve* headers from `npsim` utilities.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: npsim fails to install when TEve is not available)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

--- src/tools/src/settings.h
+++ src/tools/src/settings.h
@@ -1,48 +1,12 @@
 #ifndef NPDET_TOOLS_SETTINGS_H
 #define NPDET_TOOLS_SETTINGS_H
 
-#include "Math/Vector3D.h"
-#include "Math/Vector4D.h"
-#include "Math/VectorUtil.h"
-#include "TCanvas.h"
-#include "TLegend.h"
-#include "TMath.h"
-#include "TRandom3.h"
-#include "TFile.h"
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TH1D.h"
-#include "TH2D.h"
-#include "TTree.h"
-#include "TF1.h"
+#include <map>
+#include <string>
 
-#include <vector>
-#include <tuple>
-#include <algorithm>
-#include <iterator>
-#include <thread>
-#include <filesystem>
-namespace fs = std::filesystem;
+class TGeoNode;
 
-#include "DD4hep/Detector.h"
-#include "DD4hep/Printout.h"
-#include "DDG4/Geant4Data.h"
-#include "DDRec/CellIDPositionConverter.h"
-#include "DDRec/SurfaceManager.h"
-#include "DDRec/Surface.h"
-#include "DD4hep/DD4hepUnits.h"
-
-#include "TApplication.h"
-#include "ROOT/TFuture.hxx"
-
-#include "TGeoManager.h"
-#include "Math/DisplacementVector3D.h"
-#include "TEveManager.h"
-#include "TEveGeoNode.h"
-#include "TEveGeoShapeExtract.h"
-#include "TEveGeoShape.h"
-#include "TEveBrowser.h"
-
+using std::string;
 
 enum class mode { none, help, list, part };
 
@@ -61,26 +25,12 @@ struct settings {
   bool            list_all          = false;
   int     color          = 1;
   double  alpha          = 1;
-  std::map<std::string,int> part_name_levels;
-  std::map<std::string, int>   part_name_colors;
-  std::map<std::string,double> part_name_alphas;
+  std::map<std::string, int>    part_name_levels;
+  std::map<std::string, int>    part_name_colors;
+  std::map<std::string, double> part_name_alphas;
 };
 
 void print_daughter_nodes(TGeoNode* node, int print_depth) ;
 void run_list_mode(const settings& s);
-
-//struct settings2 {
-//  bool help           = false;
-//  bool success        = false;
-//  std::string infile  = "";
-//  std::string outfile = "detector_geometry";
-//  std::string p_name  = "";
-//  int     part_level  = -1;
-//  bool    level_set      = false;
-//  int     geo_level      = -1;
-//  bool    list_all       = false;
-//  mode    selected       = mode::list;
-//};
-////______________________________________________________________________________
 
 #endif
