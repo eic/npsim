a189393d8554a40cf698bd31f19d553f821dd2ef
Wouter Deconinck <wdconinc@gmail.com>
2026-05-26 14:59:03 -0500

    Revert "feat: use Geant4TVUserParticleHandler by default (#44)" (#64)
    ### Briefly, what does this PR introduce? Please link to any relevant
    presentations or discussions.
    This reverts commit 6c595b99dae0a7860d76722f1dd0aa49e2a8059f. It has too
    large implications in https://github.com/eic/containers/pull/257 and
    needs additional pieces..
    
    ### What is the urgency of this PR?
    - [ ] High (please describe reason below)
    - [x] Medium
    - [ ] Low
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue: stuck on this in
    https://github.com/eic/containers/pull/257)
    - [ ] New feature (issue #__)
    - [ ] Optimization (issue #__)
    - [ ] Updated documentation
    - [ ] other: __
    
    ### Please check if any of the following apply
    - [ ] This PR introduces breaking changes. Please describe changes users
    need to make below.
    - [ ] This PR changes default behavior. Please describe changes below.
    - [ ] AI was used in preparing this PR. Please describe usage below.

--- src/dd4pod/python/npsim.py
+++ src/dd4pod/python/npsim.py
@@ -44,9 +44,6 @@ if __name__ == "__main__":
     return None
   RUNNER.physics.setupUserPhysics(setupCerenkov)
 
-  # Define tracking region using tracking_volume defined in the parallel world of the geometry
-  RUNNER.part.userParticleHandler = "Geant4TVUserParticleHandler"
-
   # Disable warnings for unstable resonances with off-shell mass
   if hasattr(RUNNER.physics, "ESeverity"):
     RUNNER.physics.ESeverity = "IgnoreTheIssue"
